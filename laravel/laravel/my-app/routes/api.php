33a4eea631a01a9c9544b6f525b25ddfad1d695b
OrnellaMarion <168301329+NellaOne@users.noreply.github.com>
2026-01-26 19:13:21 +0300

    commit-3

--- laravel/laravel/my-app/routes/api.php
+++ laravel/laravel/my-app/routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Api\AuthController;
 
 Route::prefix('auth')->group(function () {
     Route::post('/signup', [AuthController::class, 'signup']);
+    Route::post('/register', [AuthController::class, 'signup']); // Ajout de la route conventionnelle
     Route::post('/login', [AuthController::class, 'login']);
     Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
     Route::put('/profile', [AuthController::class, 'updateProfile'])->middleware('auth:sanctum');
